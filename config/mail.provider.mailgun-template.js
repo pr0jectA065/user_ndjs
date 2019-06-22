b8b41af6e4266fb42cdf79df6501dc84e0b079d2
bxpatel <i.do.prefer@gmail.com>
2019-06-21 21:15:34 -0700

    mailgun config email template instead of real config file

---
+++ config/mail.provider.mailgun-template.js
@@ -0,0 +1,7 @@
+var mailgunConfig={
+    api_key:'aaaaaa11111bbbbbbbb22222222ccccccc33333333-11111111',
+    domain:'mydomain11111111fffffff22222ffff3333330.mailgun.org',
+    from:'ourdomainemail@domain.com'
+};
+
+module.exports={mailgunConfig};
