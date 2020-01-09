cc0f1606e579fbe8877cab9a83059cd893293281
Alpeshkumar Patel <Alpeshkumar.Patel@phoenix.edu>
2020-01-09 16:04:35 -0700

    adding something better in mongo

--- config/config.db.js
+++ config/config.db.js
@@ -1,11 +1,19 @@
 var dbConfig = {
     development: {
-        url: 'mongodb://3.135.62.9:27017/pr0jectA'
+        url: 'mongodb://3.135.62.9:27017/pr0jectA',
+        hostname: '3.135.62.9',
+        port: '27017',
+        database: 'pr0jectA',
+        username: 'testli',
+        password: 'testli2020!'
+
     },
     getDbPath: function (env) {
         switch (env) {
             case 'development':
-                return dbConfig.development.url;
+                return 'mongodb://' + dbConfig.development.username + ":" + dbConfig.development.password + "@" + dbConfig.development.hostname + ":" + dbConfig.development.port + "/" + dbConfig.development.database;
+            case 'dev':
+                return 'mongodb://' + dbConfig.development.username + ":" + dbConfig.development.password + "@" + dbConfig.development.hostname + ":" + dbConfig.development.port + "/" + dbConfig.development.database;
         }
     }
 
